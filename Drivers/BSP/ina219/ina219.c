19096c264ae5bb1cb62921445d82f62a6ea34a4a
Konstantin Borissov <32213296+konstantinbsv@users.noreply.github.com>
2020-02-12 12:58:07 -0800

    Create INA219 source and header file

---
+++ Drivers/BSP/ina219/ina219.c
@@ -0,0 +1,9 @@
+/*
+ * ina219.c
+ *
+ *  Created on: Feb 12, 2020
+ *      Author: Konstantin Borissov
+ */
+
+
+#include "ina219.h"
---
+++ Drivers/BSP/ina219/ina219.h
@@ -0,0 +1,12 @@
+/*
+ * ina219.h
+ *
+ *  Created on: Feb 12, 2020
+ *      Author: Konstantin Borissov
+ */
+
+#ifndef BSP_INA219_INA219_H_
+#define BSP_INA219_INA219_H_
+
+
+#endif /* BSP_INA219_INA219_H_ */
