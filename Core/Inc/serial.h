545c721b3e1a5e0402c3797b38394f85a03bdf1a
Konstantin Borissov <32213296+konstantinbsv@users.noreply.github.com>
2020-02-18 15:42:49 -0800

    Add function to transmit char array over serial

--- Core/Inc/serial.h
+++ Core/Inc/serial.h
@@ -8,7 +8,17 @@
 #ifndef INC_SERIAL_H_
 #define INC_SERIAL_H_
 
+/* Includes */
+#include "stm32f4xx_hal.h"
+#include "stm32f4xx_hal_uart.h"
+#include <string.h>		// standard C library, contains strcopy and strlen
+#include <stdio.h>
+
+/* Constants */
+#define SERIAL_TIMEOUT 1000 // used as timeout for HAL UART functions
+
 /* Function Prototypes */
-HAL_StatusTypeDef InitializeUART (UART_HandleTypeDef _serial_handle);
+HAL_StatusTypeDef InitializeUART(UART_HandleTypeDef *serial_handle);
+HAL_StatusTypeDef UARTPrintCharArray(char data[]);
 
 #endif /* INC_SERIAL_H_ */
--- Core/Src/serial.c
+++ Core/Src/serial.c
@@ -2,7 +2,7 @@
  * serial.c
  *
  *  Created on: Feb. 18, 2020
- *      Author: Konstantin
+ *      Author: Konstantin Borissov
  */
 
 
@@ -10,10 +10,14 @@
 
 static UART_HandleTypeDef *serial_handle = NULL;
 
-HAL_StatusTypeDef InitializeUART (UART_HandleTypeDef _serial_handle) {
-	serial_handle = _serial_handle;
+HAL_StatusTypeDef InitializeUART(UART_HandleTypeDef *handle) {
+	serial_handle = handle;
 
 	return HAL_OK;
 }
 
-HAL_StatusTypeDef
+HAL_StatusTypeDef UARTPrintCharArray(char data[]) {
+	HAL_StatusTypeDef status = HAL_UART_Transmit(serial_handle, (uint8_t*) data, strlen(data), SERIAL_TIMEOUT);
+
+	return status;
+}
