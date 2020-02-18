b77c5c4f51da9b26735c4d6e56d56b8ed0d70f99
Konstantin Borissov <32213296+konstantinbsv@users.noreply.github.com>
2020-02-18 14:29:16 -0800

    Add library for custom serial functions

---
+++ Core/Inc/serial.h
@@ -0,0 +1,13 @@
+/*
+ * serial.h
+ *
+ *  Created on: Feb. 18, 2020
+ *      Author: Konstantin
+ */
+
+#ifndef INC_SERIAL_H_
+#define INC_SERIAL_H_
+
+
+
+#endif /* INC_SERIAL_H_ */
---
+++ Core/Src/serial.c
@@ -0,0 +1,10 @@
+/*
+ * serial.c
+ *
+ *  Created on: Feb. 18, 2020
+ *      Author: Konstantin
+ */
+
+
+#include <serial.h>
+
