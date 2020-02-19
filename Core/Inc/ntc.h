cfd43fdcf31cf64015aa5429778517443bde15f6
Konstantin Borissov <32213296+konstantinbsv@users.noreply.github.com>
2020-02-19 13:59:42 -0800

    Add function to read several ADC channels

--- Core/Inc/ntc.h
+++ Core/Inc/ntc.h
@@ -8,6 +8,21 @@
 #ifndef INC_NTC_H_
 #define INC_NTC_H_
 
+/* Includes */
+#include "stm32f4xx_hal.h"
+#include "stm32f4xx_hal_adc.h"
+#include <math.h>
 
+/* Constants */
+#define ADC_TIMEOUT 	1000 			// maximum delay for ADC conversion
+#define SAMPLINGTIME 	ADC_SAMPLETIME_3CYCLES
+#define NTC1_CHANNEL	ADC_CHANNEL_4 	// ADC1_IN4
+#define NTC2_CHANNEL 	ADC_CHANNEL_1	// ADC1_IN1
+#define NTC3_CHANNEL 	ADC_CHANNEL_0	// ADC1_IN0
+
+
+/* Function Prototypes */
+HAL_StatusTypeDef InitializeADC (ADC_HandleTypeDef *handle);
+uint32_t ReadNTC(int8_t ntc_channel);
 
 #endif /* INC_NTC_H_ */
--- Core/Src/ntc.c
+++ Core/Src/ntc.c
@@ -5,4 +5,36 @@
  *      Author: Konstantin Borissov
  */
 
+#include "ntc.h"
 
+static ADC_HandleTypeDef *adc_handle = NULL;
+
+/**
+ * @brief Initialize ADC handle
+ *
+ * @return HAL_OK
+ */
+HAL_StatusTypeDef InitializeADC (ADC_HandleTypeDef *handle) {
+	adc_handle = handle;
+
+
+	return HAL_OK;
+}
+
+uint32_t ReadNTC(int8_t ntc_channel) {
+	uint32_t adc_value;
+
+	ADC_ChannelConfTypeDef sConfig = {0};
+	sConfig.Channel 		= ntc_channel;
+	sConfig.SamplingTime 	= SAMPLINGTIME;
+	sConfig.Rank			= 1;
+
+	HAL_ADC_ConfigChannel(adc_handle, &sConfig); 		// Configure channel
+	HAL_ADC_Start(adc_handle);							// start ADC
+	HAL_ADC_PollForConversion(adc_handle, ADC_TIMEOUT);	// wait for conversion to complete
+
+	adc_value = HAL_ADC_GetValue(adc_handle);			// get value from ADC
+	HAL_ADC_Stop(adc_handle);							// stop ADC
+
+	return adc_value;
+}
