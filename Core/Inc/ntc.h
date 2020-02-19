c1dea778a21e1c36a53a8df0a2ddbcd4a8c18483
Konstantin Borissov <32213296+konstantinbsv@users.noreply.github.com>
2020-02-19 09:45:57 -0800

    Add library to reading NTC thermistors

---
+++ Core/Inc/ntc.h
@@ -0,0 +1,13 @@
+/*
+ * ntc.h
+ *
+ *  Created on: Feb. 19, 2020
+ *      Author: Konstantin Borissov
+ */
+
+#ifndef INC_NTC_H_
+#define INC_NTC_H_
+
+
+
+#endif /* INC_NTC_H_ */
---
+++ Core/Src/ntc.c
@@ -0,0 +1,8 @@
+/*
+ * ntc.c
+ *
+ *  Created on: Feb. 19, 2020
+ *      Author: Konstantin Borissov
+ */
+
+
