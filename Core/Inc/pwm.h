33be1f0f464c256bcfd9008e76f4f907cbbf97b2
Konstantin Borissov <32213296+konstantinbsv@users.noreply.github.com>
2020-02-20 13:18:11 -0800

    Add functions to initialize timer handle and to set PWM duty cycle

--- Core/Inc/pwm.h
+++ Core/Inc/pwm.h
@@ -8,6 +8,17 @@
 #ifndef INC_PWM_H_
 #define INC_PWM_H_
 
+/* Includes */
+#include "stm32f4xx_hal.h"
+#include "stm32f4xx_hal_gpio.h"
+#include "stm32f4xx_hal_tim.h"
 
+/* Defines */
+#define SCP1_CHANNEL TIM_CHANNEL_2
+#define
+
+/* Function Prototypes */
+HAL_StatusTypeDef InitializeTIM(TIM_HandleTypeDef *handle);
+HAL_StatusTypeDef SetPWM(uint32_t channel, uint8_t duty_cycle);
 
 #endif /* INC_PWM_H_ */
--- Core/Src/pwm.c
+++ Core/Src/pwm.c
@@ -5,4 +5,25 @@
  *      Author: Konstantin Borissov
  */
 
+#include "pwm.h"
 
+static TIM_HandleTypeDef *timer_handle = NULL;
+
+/**
+ * @brief Initialize TIM handle
+ *
+ * @return HAL_OK
+ */
+HAL_StatusTypeDef InitializeTIM(TIM_HandleTypeDef *handle) {
+	timer_handle = handle;
+
+	return HAL_OK;
+}
+
+HAL_StatusTypeDef SetPWM(uint32_t channel, uint8_t duty_cycle) {
+
+	HAL_StatusTypeDef status = HAL_TIM_PWM_Start(timer_handle, channel);	// start PWM
+	timer_handle->Instance->CCR2 = duty_cycle;								// set PWM duty cycle
+
+	return status;
+}
