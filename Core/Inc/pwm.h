42fdceb35f02a5ac73ef8131657b5d695966e48e
Konstantin Borissov <32213296+konstantinbsv@users.noreply.github.com>
2020-02-20 14:09:43 -0800

    Create functions to control PWM of each SCP

--- Core/Inc/pwm.h
+++ Core/Inc/pwm.h
@@ -9,16 +9,22 @@
 #define INC_PWM_H_
 
 /* Includes */
+#include "stm32f446xx.h"
 #include "stm32f4xx_hal.h"
 #include "stm32f4xx_hal_gpio.h"
 #include "stm32f4xx_hal_tim.h"
 
 /* Defines */
-#define SCP1_CHANNEL TIM_CHANNEL_2
-#define
+
+enum {	// SCP Timer Channels
+	SCP1_CHANNEL = TIM_CHANNEL_2,
+	SCP2_CHANNEL = TIM_CHANNEL_1,
+	SCP3_CHANNEL = TIM_CHANNEL_3
+};
+
 
 /* Function Prototypes */
-HAL_StatusTypeDef InitializeTIM(TIM_HandleTypeDef *handle);
-HAL_StatusTypeDef SetPWM(uint32_t channel, uint8_t duty_cycle);
+HAL_StatusTypeDef InitializeTIM(TIM_HandleTypeDef *handle1, TIM_HandleTypeDef *handle2, TIM_HandleTypeDef *handle3);
+HAL_StatusTypeDef SetPWM_SCP1(uint8_t duty_cycle);
 
 #endif /* INC_PWM_H_ */
--- Core/Src/pwm.c
+++ Core/Src/pwm.c
@@ -7,23 +7,43 @@
 
 #include "pwm.h"
 
-static TIM_HandleTypeDef *timer_handle = NULL;
+TIM_HandleTypeDef *handleSCP1;
+TIM_HandleTypeDef *handleSCP2;
+TIM_HandleTypeDef *handleSCP3;
 
 /**
  * @brief Initialize TIM handle
  *
  * @return HAL_OK
  */
-HAL_StatusTypeDef InitializeTIM(TIM_HandleTypeDef *handle) {
-	timer_handle = handle;
+HAL_StatusTypeDef InitializeTIM(TIM_HandleTypeDef *handle1, TIM_HandleTypeDef *handle2, TIM_HandleTypeDef *handle3) {
+	handleSCP1 = handle1;
+	handleSCP2 = handle2;
+	handleSCP3 = handle3;
 
 	return HAL_OK;
 }
 
-HAL_StatusTypeDef SetPWM(uint32_t channel, uint8_t duty_cycle) {
+HAL_StatusTypeDef SetPWM_SCP1(uint8_t duty_cycle) {
 
-	HAL_StatusTypeDef status = HAL_TIM_PWM_Start(timer_handle, channel);	// start PWM
-	timer_handle->Instance->CCR2 = duty_cycle;								// set PWM duty cycle
+	HAL_StatusTypeDef status = HAL_TIM_PWM_Start(handleSCP1, SCP1_CHANNEL);	// start PWM
+	handleSCP1->Instance->CCR2 = duty_cycle;								// set PWM duty cycle
+
+	return status;
+}
+
+HAL_StatusTypeDef SetPWM_SCP2(uint8_t duty_cycle) {
+
+	HAL_StatusTypeDef status = HAL_TIM_PWM_Start(handleSCP1, SCP2_CHANNEL);	// start PWM
+	handleSCP1->Instance->CCR1 = duty_cycle;								// set PWM duty cycle
+
+	return status;
+}
+
+HAL_StatusTypeDef SetPWM_SCP3(uint8_t duty_cycle) {
+
+	HAL_StatusTypeDef status = HAL_TIM_PWM_Start(handleSCP1, SCP3_CHANNEL);	// start PWM
+	handleSCP1->Instance->CCR3 = duty_cycle;								// set PWM duty cycle
 
 	return status;
 }
