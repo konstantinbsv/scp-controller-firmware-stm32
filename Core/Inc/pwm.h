b580dc615b58bf8652c6b3216861c3032c42ac02
Konstantin Borissov <32213296+konstantinbsv@users.noreply.github.com>
2020-02-20 14:12:48 -0800

    Add function prototypes

--- Core/Inc/pwm.h
+++ Core/Inc/pwm.h
@@ -26,5 +26,7 @@ enum {	// SCP Timer Channels
 /* Function Prototypes */
 HAL_StatusTypeDef InitializeTIM(TIM_HandleTypeDef *handle1, TIM_HandleTypeDef *handle2, TIM_HandleTypeDef *handle3);
 HAL_StatusTypeDef SetPWM_SCP1(uint8_t duty_cycle);
+HAL_StatusTypeDef SetPWM_SCP2(uint8_t duty_cycle);
+HAL_StatusTypeDef SetPWM_SCP3(uint8_t duty_cycle);
 
 #endif /* INC_PWM_H_ */
