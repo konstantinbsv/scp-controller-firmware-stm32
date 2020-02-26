24583a9bdb0525e25271c84af90576b8862b0c17
Konstantin Borissov <32213296+konstantinbsv@users.noreply.github.com>
2020-02-26 11:30:12 -0800

    Add PID library

---
+++ Core/Inc/pid.h
@@ -0,0 +1,30 @@
+/*
+ * pid.h
+ *
+ *  Created on: Feb. 26, 2020
+ *      Author: Konstantin Borissov
+ */
+
+#ifndef INC_PID_H_
+#define INC_PID_H_
+
+/* PID Constant Definitions */
+
+// P (Proportional) - constant
+enum {
+	SCP1_P = 75,
+	SCP2_P = 75,
+	SCP3_P = 75
+};
+
+// I (Integral) - constant
+enum {
+
+};
+
+// D (Derivative) - constant
+enum {
+
+};
+
+#endif /* INC_PID_H_ */
---
+++ Core/Src/pid.c
@@ -0,0 +1,10 @@
+/*
+ * pid.c
+ *
+ *  Created on: Feb. 26, 2020
+ *      Author: Konstantin Borissov
+ */
+
+
+#include "pid.h"
+
