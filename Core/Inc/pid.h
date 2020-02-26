972419eff9b254ea6713d88992f94f10b19397f7
Konstantin Borissov <32213296+konstantinbsv@users.noreply.github.com>
2020-02-26 11:33:12 -0800

    Add anonymous enums for PID constants

--- Core/Inc/pid.h
+++ Core/Inc/pid.h
@@ -8,23 +8,33 @@
 #ifndef INC_PID_H_
 #define INC_PID_H_
 
+/* Includes */
+#include "pwm.h"
+
 /* PID Constant Definitions */
 
 // P (Proportional) - constant
 enum {
 	SCP1_P = 75,
-	SCP2_P = 75,
+	SCP2_P = 20,
 	SCP3_P = 75
 };
 
 // I (Integral) - constant
 enum {
-
+	SCP1_I = 0,
+	SCP2_I = 0,
+	SCP3_I = 0,
 };
 
 // D (Derivative) - constant
 enum {
-
+	SCP1_D = 0,
+	SCP2_D = 0,
+	SCP3_D = 0,
 };
+
+/* Function Prototypes */
+
 
 #endif /* INC_PID_H_ */
