78b6adb12718bb83d6cdace7b0973327a068c282
Konstantin Borissov <32213296+konstantinbsv@users.noreply.github.com>
2020-03-06 14:37:30 -0800

    Create state machine

---
+++ Core/Inc/state_machine.h
@@ -0,0 +1,19 @@
+/*
+ * state_machine.h
+ *
+ *  Created on: Mar. 6, 2020
+ *      Author: Konstantin Borissov
+ */
+
+#ifndef INC_STATE_MACHINE_H_
+#define INC_STATE_MACHINE_H_
+
+/* Includes */
+#include "pid.h"
+#include "pwm.h"
+#include "ina219.h"
+
+/* Constants */
+#include
+
+#endif /* INC_STATE_MACHINE_H_ */
---
+++ Core/Src/state_machine.c
@@ -0,0 +1,11 @@
+/*
+ * state_machine.c
+ *
+ *  Created on: Mar. 6, 2020
+ *      Author: Konstantin Borissov
+ */
+
+
+uint8_t controller() {
+
+}
