19633ff48ad6967b9dc6274c06dda981298b2996
Konstantin Borissov <32213296+konstantinbsv@users.noreply.github.com>
2020-03-06 15:05:36 -0800

    Create outline and variables

--- Core/Inc/state_machine.h
+++ Core/Inc/state_machine.h
@@ -9,11 +9,19 @@
 #define INC_STATE_MACHINE_H_
 
 /* Includes */
+#include "stdbool.h"
 #include "pid.h"
 #include "pwm.h"
 #include "ina219.h"
 
 /* Constants */
-#include
+#define MAX_CURRENT_MA	750		// max allowable current before over-current state is entered
+#define MAX_OC_TIME_MS	500		// max allowable time in over-current state before current limiting state is entered
+
+/* Variables */
+static bool 	over_current_state[3];
+static bool 	current_limit_state[3];
+static uint8_t	safe_PWM[3];			// to store max safe PWM duty cycle at point of entry into over-current state
+static uint16_t	oc_time[3];				// to store time spent in over-current state
 
 #endif /* INC_STATE_MACHINE_H_ */
--- Core/Src/state_machine.c
+++ Core/Src/state_machine.c
@@ -5,7 +5,29 @@
  *      Author: Konstantin Borissov
  */
 
+#include "state_machine.h"
 
-uint8_t controller() {
+/* Initialize variables */
+static bool 	over_current_state[3] 	= {false};
+static bool 	current_limit_state[3]	= {false};
+static uint8_t	safe_PWM[3] = {0};
+static uint16_t	oc_time[3]	= {0};
+
+
+/**
+ * @param scp_index 	index of SCP fiber
+ * @param current temp 	current temperature of scp fiber
+ *
+ * @return PWM duty cycle output from 0% to 100%
+ */
+uint8_t controller(uint8_t scp_index, float current_temp) {
+
+	// get PWM output from PID
+
+	// get current, if current > MAX_CURRENT, start count, save safePWM, set OC flag
+			// if count start, and MAX_OC_TIME elapsed, go into current_limit mode (set safePWM)
+			// if current < MAX_CURRENT, stop count
+
+		// if PWM_output < last_safe_PWM, exit current_limit mode
 
 }
