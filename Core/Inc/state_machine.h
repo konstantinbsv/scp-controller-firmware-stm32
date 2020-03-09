599ab6310d0a6573416d66a59797992fbb7db647
Konstantin Borissov <32213296+konstantinbsv@users.noreply.github.com>
2020-03-09 08:49:26 -0700

    Create algorithm for calcualting maximum allowable duty cycle

--- Core/Inc/state_machine.h
+++ Core/Inc/state_machine.h
@@ -15,13 +15,17 @@
 #include "ina219.h"
 
 /* Constants */
-#define MAX_CURRENT_MA	750		// max allowable current before over-current state is entered
+#define MAX_CURRENT_MA	350		// max allowable current before over-current state is entered
 #define MAX_OC_TIME_MS	500		// max allowable time in over-current state before current limiting state is entered
+#define HYSTERESIS		5		// percent duty cycle commanded by PID under last safe PWM to return to normal state
 
 /* Variables */
 static bool 	over_current_state[3];
 static bool 	current_limit_state[3];
-static uint8_t	safe_PWM[3];			// to store max safe PWM duty cycle at point of entry into over-current state
-static uint16_t	oc_time[3];				// to store time spent in over-current state
+static uint8_t	max_safe_PWM[3];			// to store calculated maximum PWM duty cycle at point of entry into over-current state
+static uint32_t	oc_start_time[3];		// to store time when over-current state started
+
+/* Function prototypes */
+uint8_t fsm_controller(uint8_t scp_index, float current_temp);
 
 #endif /* INC_STATE_MACHINE_H_ */
--- Core/Src/state_machine.c
+++ Core/Src/state_machine.c
@@ -8,26 +8,55 @@
 #include "state_machine.h"
 
 /* Initialize variables */
-static bool 	over_current_state[3] 	= {false};
+static bool 	over_current_state[3]	= {false};
 static bool 	current_limit_state[3]	= {false};
-static uint8_t	safe_PWM[3] = {0};
-static uint16_t	oc_time[3]	= {0};
+static uint8_t	max_safe_PWM[3] 		= {0};
+static uint32_t	oc_start_time[3]		= {0};
 
 
 /**
+ * @brief Finite state machine controller
+ *
  * @param scp_index 	index of SCP fiber
  * @param current temp 	current temperature of scp fiber
  *
- * @return PWM duty cycle output from 0% to 100%
+ * @retval PWM duty cycle output from 0% to 100%
  */
-uint8_t controller(uint8_t scp_index, float current_temp) {
+uint8_t fsm_controller(uint8_t scp_index, float current_temp) {
 
-	// get PWM output from PID
+	// get PWM duty cycle output from PID
+	uint8_t duty_cycle = updatePID(scp_index, current_temp); // read duty cycle commanded by PID algorithm
 
-	// get current, if current > MAX_CURRENT, start count, save safePWM, set OC flag
-			// if count start, and MAX_OC_TIME elapsed, go into current_limit mode (set safePWM)
-			// if current < MAX_CURRENT, stop count
+	// get current
+	float current = GetCurrent_mA(INA219_ADDRESSES[scp_index]); // get INA219 address from address array in ina219 library
 
-		// if PWM_output < last_safe_PWM, exit current_limit mode
+	// if exceeding maximum allowable current and in normal state
+	if (current > MAX_CURRENT_MA && !over_current_state[scp_index] && !current_limit_state[scp_index]) {
+			max_safe_PWM[scp_index] = (MAX_CURRENT_MA / current ) * duty_cycle;		// calculate max safe PWM
+			oc_start_time[scp_index] = HAL_GetTick();								// save start time
+			over_current_state[scp_index] = true; 									// enter over-current state
+	}
+	// if maximum over-current time has elapsed, go into current limit state
+	if (( HAL_GetTick() - oc_start_time[scp_index] ) > MAX_OC_TIME_MS && over_current_state[scp_index]) {
+		// duty_cycle = max_safe_PWM[scp_index];	// set duty cycle as last safe duty cycle
+		over_current_state[scp_index] = false; 	// exit over-current state
+		current_limit_state[scp_index] = true; 	// enter current limit state
+	}
 
+	// if not in normal state
+	if (over_current_state[scp_index] || current_limit_state[scp_index]) {
+		// and if PID commands PWM duty cycle below last safe PWM minus hysteresis
+		if (duty_cycle < max_safe_PWM[scp_index] - HYSTERESIS) {
+			over_current_state[scp_index]	= false;	// exit over-current state
+			current_limit_state[scp_index]	= false;	// exit current limit state
+			// FSM will now be in normal state
+		}
+	}
+
+	// set duty cycle according to state
+	duty_cycle = current_limit_state[scp_index] ? max_safe_PWM[scp_index] : duty_cycle;
+	// output duty cycle
+	SetPWM(scp_index, duty_cycle);
+
+	return duty_cycle;
 }
